8f131fbfb7a0e4808d056d0622f8ce88b12f5460
renhe <renhe228@gmail.com>
2018-12-18 18:14:39 +0800

    1、个人中心重新布局 2、登录未登录状态显示逻辑

--- app/src/main/java/com/chengsheng/cala/htcm/module/fragments/MineFragment.java
+++ app/src/main/java/com/chengsheng/cala/htcm/module/fragments/MineFragment.java
@@ -1,6 +1,8 @@
 package com.chengsheng.cala.htcm.module.fragments;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,34 +13,36 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.chengsheng.cala.htcm.constant.GlobalConstant;
+import com.allen.library.SuperTextView;
 import com.chengsheng.cala.htcm.HTCMApp;
 import com.chengsheng.cala.htcm.R;
-import com.chengsheng.cala.htcm.model.businesslogic.DataFlow;
-import com.chengsheng.cala.htcm.protocol.childmodela.UserInfo;
-import com.chengsheng.cala.htcm.network.AccountService;
-import com.chengsheng.cala.htcm.network.MyRetrofit;
+import com.chengsheng.cala.htcm.constant.GlobalConstant;
+import com.chengsheng.cala.htcm.module.account.LoginActivity;
 import com.chengsheng.cala.htcm.module.activitys.AccountSettingActivity;
 import com.chengsheng.cala.htcm.module.activitys.ExamOrderFormActivity;
+import com.chengsheng.cala.htcm.module.activitys.FamilyManageActivity;
+import com.chengsheng.cala.htcm.module.activitys.MyCollectionActivity;
+import com.chengsheng.cala.htcm.module.activitys.MyDevicesActivity;
 import com.chengsheng.cala.htcm.module.activitys.ServiceMessageActivity;
 import com.chengsheng.cala.htcm.module.activitys.ServiceOrderActivity;
-import com.chengsheng.cala.htcm.adapter.MineItemBaseAdapter;
+import com.chengsheng.cala.htcm.module.activitys.SettingActivity;
+import com.chengsheng.cala.htcm.network.AccountService;
+import com.chengsheng.cala.htcm.network.MyRetrofit;
+import com.chengsheng.cala.htcm.protocol.childmodela.UserInfo;
+import com.chengsheng.cala.htcm.utils.ActivityUtil;
+import com.chengsheng.cala.htcm.utils.UserUtil;
 import com.facebook.drawee.view.SimpleDraweeView;
-
-import java.util.List;
-import java.util.Map;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
 
-
 /**
- * Author:
+ * Author: 蔡浪
  * CreateDate:
  * Description: 我的模块
  */
@@ -46,32 +50,23 @@ public class MineFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    private String mParam1;
-    private String mParam2;
-
     private OnFragmentInteractionListener mListener;
 
     private TextView medicalExamOrderText;
     private TextView userNameText;
     private TextView userCellphoneNum;
-    private ImageView inputPersonalInfo;
     private SimpleDraweeView userIcon;
     private FrameLayout messageIconContainerMine;
+    private RelativeLayout layoutUpdateUserInfo;
     private ImageView currentHasMessageMine;
+    private SuperTextView stv_family_manager;
+    private SuperTextView stv_collection;
+    private SuperTextView stv_device;
+    private SuperTextView stv_setting;
+    private SuperTextView stv_contact;
 
     private Retrofit retrofit;
     private HTCMApp app;
-
-    private int[] iconsA = new int[]{R.mipmap.jiarengaunli, R.mipmap.shoucang, R.mipmap.tuikuanjilu, R.mipmap.wodeshebei};
-    private int[] iconsB = new int[]{R.mipmap.changjianwenti, R.mipmap.shezhi, R.mipmap.lianxikefu};
-    private String[] titleA = new String[]{"家人管理", "收藏", "退款记录", "我的设备"};
-    private String[] titleB = new String[]{"常见问题", "设置", "联系客服"};
-    private String[] typeA = new String[]{"GENERAL", "GENERAL", "GENERAL", "GENERAL"};
-    private String[] typeB = new String[]{"GENERAL", "GENERAL", "TEL"};
-
-    public MineFragment() {
-        // Required empty public constructor
-    }
 
     public static MineFragment newInstance(String param1, String param2) {
         MineFragment fragment = new MineFragment();
@@ -82,12 +77,16 @@ public class MineFragment extends Fragment {
         return fragment;
     }
 
+    public MineFragment() {
+
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            String mParam1 = getArguments().getString(ARG_PARAM1);
+            String mParam2 = getArguments().getString(ARG_PARAM2);
         }
 
         app = HTCMApp.create(getContext());
@@ -98,35 +97,81 @@ public class MineFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_mine, container, false);
 
-        ListView groupA = rootView.findViewById(R.id.mine_group_a);
-        ListView groupB = rootView.findViewById(R.id.mine_group_b);
         medicalExamOrderText = rootView.findViewById(R.id.medical_exam_order_text);//体检订单文本
         TextView serviceOrderText = rootView.findViewById(R.id.service_order_text);
         userNameText = rootView.findViewById(R.id.user_name_text);
         userCellphoneNum = rootView.findViewById(R.id.user_cellphone_num);
-        inputPersonalInfo = rootView.findViewById(R.id.input_personal_info);
         userIcon = rootView.findViewById(R.id.user_icon);
         messageIconContainerMine = rootView.findViewById(R.id.message_icon_container_mine);
         currentHasMessageMine = rootView.findViewById(R.id.current_has_message_mine);
+        layoutUpdateUserInfo = rootView.findViewById(R.id.layout_update_user_info);
+        stv_family_manager = rootView.findViewById(R.id.stv_family_manager);
+        stv_contact = rootView.findViewById(R.id.stv_contact);
+        stv_device = rootView.findViewById(R.id.stv_device);
+        stv_setting = rootView.findViewById(R.id.stv_setting);
+        stv_collection = rootView.findViewById(R.id.stv_collection);
 
-
-        updateInfo();
-
-        messageIconContainerMine.setOnClickListener(new View.OnClickListener() {
+        //修改用户信息
+        layoutUpdateUserInfo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), ServiceMessageActivity.class);
-                getContext().startActivity(intent);
+            public void onClick(View view) {
+                if (!UserUtil.isLogin()) {
+                    ActivityUtil.Companion.startActivity(getContext(), new LoginActivity());
+                }
             }
         });
 
-        List<Map<String, String>> dataGroupA = DataFlow.minePageItemModelA(4, iconsA, titleA, typeA);
-        List<Map<String, String>> dataGroupB = DataFlow.minePageItemModelA(3, iconsB, titleB, typeB);
+        //家人管理
+        stv_family_manager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityUtil.Companion.startActivityWithLoginStatus(getContext(), new FamilyManageActivity());
+            }
+        });
 
-        groupA.setAdapter(new MineItemBaseAdapter(getContext(), dataGroupA));
-        groupB.setAdapter(new MineItemBaseAdapter(getContext(), dataGroupB));
+        //我的收藏
+        stv_collection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityUtil.Companion.startActivityWithLoginStatus(getContext(), new MyCollectionActivity());
+            }
+        });
 
+        //我的设备
+        stv_device.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityUtil.Companion.startActivityWithLoginStatus(getContext(), new MyDevicesActivity());
+            }
+        });
 
+        //设置
+        stv_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityUtil.Companion.startActivityWithLoginStatus(getContext(), new SettingActivity());
+            }
+        });
+
+        //联系客服
+        stv_contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contactService();
+            }
+        });
+
+        updateUserInfo();
+
+        //查看消息
+        messageIconContainerMine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityUtil.Companion.startActivityWithLoginStatus(getContext(), new ServiceMessageActivity());
+            }
+        });
+
+        //服务订单
         serviceOrderText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -138,36 +183,32 @@ public class MineFragment extends Fragment {
         return rootView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    private void contactService() {
+        AlertDialog alertDialog;
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("拨号提示");
+        builder.setMessage("确认向客服中心拨打电话?");
+        builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                Uri data = Uri.parse("tel:" + "400-028-3020");
+                intent.setData(data);
+                startActivity(intent);
+            }
+        });
+
+        builder.setNegativeButton("暂不", null);
+
+        alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void updateUserInfo() {
+        if (!UserUtil.isLogin()) {
+            return;
         }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
-
-    private void updateInfo() {
         if (retrofit == null) {
             retrofit = MyRetrofit.createInstance().createURL(GlobalConstant.TEST_URL);
         }
@@ -197,7 +238,8 @@ public class MineFragment extends Fragment {
     void setViews(final UserInfo userInfo) {
         userIcon.setImageURI(userInfo.getAvatar_url());
         userNameText.setText(userInfo.getNickname());
-        userCellphoneNum.setText(userInfo.getPhone_number());
+        String phoneNumber = userInfo.getPhone_number().replaceAll("(\\d{3})\\d{4}(\\d{4})", "$1****$2");
+        userCellphoneNum.setText(phoneNumber);
 
         medicalExamOrderText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -208,16 +250,56 @@ public class MineFragment extends Fragment {
             }
         });
 
-        inputPersonalInfo.setOnClickListener(new View.OnClickListener() {
+        //修改用户信息
+        layoutUpdateUserInfo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), AccountSettingActivity.class);
+            public void onClick(View view) {
                 Bundle bundle = new Bundle();
                 bundle.putSerializable("USER_INFO", userInfo);
-                intent.putExtras(bundle);
-                getContext().startActivity(intent);
+                ActivityUtil.Companion.startActivity(getContext(),
+                        new AccountSettingActivity(), bundle);
             }
         });
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (UserUtil.isLogin()) {
+            stv_family_manager.setVisibility(View.VISIBLE);
+            stv_collection.setVisibility(View.VISIBLE);
+            stv_device.setVisibility(View.VISIBLE);
+        } else {
+            stv_family_manager.setVisibility(View.GONE);
+            stv_collection.setVisibility(View.GONE);
+            stv_device.setVisibility(View.GONE);
+        }
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
 }

cc8c3a779f59612dc61ae4e4037b569c72df45eb
renhe <renhe228@gmail.com>
2018-12-20 16:23:15 +0800

    封装分享弹出框

--- app/src/main/java/com/chengsheng/cala/htcm/adapter/FeatureServiceAdapter.kt
+++ app/src/main/java/com/chengsheng/cala/htcm/adapter/FeatureServiceAdapter.kt
@@ -1,17 +1,20 @@
 package com.chengsheng.cala.htcm.adapter
 
 import android.view.View
+import android.widget.RelativeLayout
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.chengsheng.cala.htcm.R
 import com.chengsheng.cala.htcm.manager.FrescoManager
+import com.chengsheng.cala.htcm.module.find.ServiceDetailActivity
 import com.chengsheng.cala.htcm.protocol.RecommendProProtocol
+import com.chengsheng.cala.htcm.utils.ActivityUtil
 import com.facebook.drawee.view.SimpleDraweeView
 
 /**
  * Author: 任和
  * CreateDate: 2018/12/19 6:16 PM
- * Description: 发现页特色服务列表Adapter
+ * Description: 特色服务列表Adapter
  */
 class FeatureServiceAdapter(data: List<RecommendProProtocol.ItemsBean.RecommendBean>) :
         BaseQuickAdapter<RecommendProProtocol.ItemsBean.RecommendBean>(R.layout.item_feature_service, data) {
@@ -19,6 +22,7 @@ class FeatureServiceAdapter(data: List<RecommendProProtocol.ItemsBean.RecommendB
     override fun convert(helper: BaseViewHolder?, item: RecommendProProtocol.ItemsBean.RecommendBean?) {
         val ivProCover: SimpleDraweeView = helper?.getView(R.id.ivProCover)!!
         val featureDivide: View = helper.getView(R.id.featureDivide)
+        val layoutFeatureService: RelativeLayout = helper.getView(R.id.layoutFeatureService)
 
         FrescoManager.loadRoundImage(mContext, ivProCover, item?.cover_photo, 4f)
         helper.setText(R.id.tvProSaleNum, (item?.current_sales_num).toString() + "人已享受")
@@ -30,6 +34,11 @@ class FeatureServiceAdapter(data: List<RecommendProProtocol.ItemsBean.RecommendB
             View.INVISIBLE
         } else {
             View.VISIBLE
+        }
+
+        //查看详情
+        layoutFeatureService.setOnClickListener {
+            ActivityUtil.startActivity(mContext, ServiceDetailActivity())
         }
     }
 }

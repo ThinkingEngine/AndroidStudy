8e608c3f94f87039d7e3255b9310e362bee79a75
renhe <renhe228@gmail.com>
2018-12-24 16:15:50 +0800

    充值页

---
+++ app/src/main/java/com/chengsheng/cala/htcm/module/user/card/DepositActivity.kt
@@ -0,0 +1,24 @@
+package com.chengsheng.cala.htcm.module.user.card
+
+import com.chengsheng.cala.htcm.R
+import com.chengsheng.cala.htcm.base.BaseActivity
+
+/**
+ * Author: 任和
+ * CreateDate: 2018/12/24 3:22 PM
+ * Description: 充值
+ */
+class DepositActivity : BaseActivity() {
+
+    override fun getLayoutId(): Int {
+        return R.layout.activity_deposit
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun getData() {
+
+    }
+}

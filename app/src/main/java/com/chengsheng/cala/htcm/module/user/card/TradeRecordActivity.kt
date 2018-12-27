65e682083377938ab4ec6062fd834c94b8717d7d
renhe <renhe228@gmail.com>
2018-12-27 11:22:41 +0800

    会员卡删除状态下交易记录报错问题

--- app/src/main/java/com/chengsheng/cala/htcm/module/user/card/TradeRecordActivity.kt
+++ app/src/main/java/com/chengsheng/cala/htcm/module/user/card/TradeRecordActivity.kt
@@ -35,6 +35,7 @@ class TradeRecordActivity : BaseRefreshActivity<TradeRecordProtocol.ItemsBean>()
 
                 }) {
                     showError(it)
+                    fillData(ArrayList())
                 }
     }
 
